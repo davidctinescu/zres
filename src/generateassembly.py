18b578128640593315bb9ee747b521d35f6a4b1b
david <david.andrei.pisoi@gmail.com>
2024-05-07 17:25:44 +0300

    fixed bug (major)

--- src/generateassembly.py
+++ src/generateassembly.py
@@ -9,42 +9,37 @@ section .data\n"""
     variables = {}
 
     for idx, line in enumerate(lines, start=1):
-        if "{" in line or "}" in line:
-            continue
         if ";" not in line:
-            raise ValueError(f"Delimiter ';' not found in line {idx}")
-        parts = line.split(";")
-        for part in parts:
-            if "//" in part:
-                continue
-            if "global main" in part:
-                in_main = True
-                continue
-            elif in_main:
-                if "}" in part:
-                    in_main = False
-                    break
-                elif "out" in part:
-                    if "(" in part and ")" in part:
-                        string = part.strip().split("(", 1)[1].split(")", 1)[0]
-                        if string.startswith('"') and string.endswith('"'):
-                            string = string[1:-1]
-                            asm_code += f"msg_{idx} db '{string}', 0xA, 0\n"
-                        else:
-                            content = part[part.find("(") + 1:part.find(")")]
-                            if content in variables:
-                                content = variables[content]
-                            else:
-                                content = f"var_{len(variables)}"
-                            asm_code += f"var_{idx} db '{content}', 0xA, 0\n"
+            if "{" in line or "}" in line:
+                pass
+            else:
+                raise ValueError(f"Delimiter ';' not found in line {idx}")
+        if "//" in line:
+            line = line.split("//")[0]
+        if "global main" in line:
+            in_main = True
+            continue
+        elif in_main:
+            if "}" in line:
+                in_main = False
+                break
+            elif "out" in line:
+                if "(" in line and ")" in line:
+                    string = re.search(r'out\((.*?)\)', line).group(1)
+                    if string.startswith('"') and string.endswith('"'):
+                        string = string[1:-1]
+                        asm_code += f"msg_{idx} db '{string}', 0xA, 0\n"
                     else:
-                        raise ValueError(f"Line {idx} has the wrong format for outputting")
-                elif "var" in part:
-                    var_name = part.split()[1].rstrip(";")
-                    var_value = re.search(r'("[^"]*"|\d+)', part).group(1)
-                    variables[var_name] = var_value
-                elif "exit" in part:
-                    exit_code = part.strip().split(" ")[1].rstrip(';')
+                        content = variables.get(string, f"var_{len(variables)}")
+                        asm_code += f"{content} db '{string}', 0xA, 0\n"
+                else:
+                    raise ValueError(f"Line {idx} has the wrong format for outputting")
+            elif "var" in line:
+                var_name = line.split()[1].rstrip(";")
+                var_value = re.search(r'("[^"]*"|\d+)', line).group(1)
+                variables[var_name] = f"var_{idx}"
+            elif "exit" in line:
+                exit_code = line.strip().split(" ")[1].rstrip(';')
 
     asm_code += """section .text
 
@@ -53,47 +48,44 @@ _start:\n"""
     in_main = False
 
     for idx, line in enumerate(lines, start=1):
-        if "{" in line or "}" in line:
-            continue
         if ";" not in line:
-            raise ValueError(f"Delimiter ';' not found in line {idx}")
-        parts = line.split(";")
-        for part in parts:
-            if "//" in part:
-                continue
-            if "main {" in part:
-                in_main = True
-                continue
-            elif in_main:
-                if "}" in part:
-                    in_main = False
-                    break
-                elif "out" in part:
-                    if "(" in part and ")" in part:
-                        string = part.strip().split("(", 1)[1].split(")", 1)[0]
-                        if string.startswith('"') and string.endswith('"'):
-                            string = string[1:-1]
-                            asm_code += f"  mov rax, 1\n"
-                            asm_code += f"  mov rdi, 1\n"
-                            asm_code += f"  mov rsi, msg_{idx}\n"
-                            asm_code += f"  mov rdx, {len(string) + 1}\n"
-                            asm_code += f"  syscall\n\n"
-                        else:
-                            content = part[part.find("(") + 1:part.find(")")]
-                            if content in variables:
-                                content = variables[content]
-                                asm_code += f"  mov rax, 1\n"
-                                asm_code += f"  mov rdi, 1\n"
-                                asm_code += f"  mov rsi, var_{idx}\n"
-                                asm_code += f"  mov rdx, {len(content) + 1}\n"
-                                asm_code += f"  syscall\n\n"
+            if "{" in line or "}" in line:
+                pass
+            else:
+                raise ValueError(f"Delimiter ';' not found in line {idx}")
+        if "//" in line:
+            line = line.split("//")[0]
+        if "main {" in line:
+            in_main = True
+            continue
+        elif in_main:
+            if "}" in line:
+                in_main = False
+                break
+            elif "out" in line:
+                if "(" in line and ")" in line:
+                    string = re.search(r'out\((.*?)\)', line).group(1)
+                    if string.startswith('"') and string.endswith('"'):
+                        string = string[1:-1]
+                        asm_code += f"  mov rax, 1\n"
+                        asm_code += f"  mov rdi, 1\n"
+                        asm_code += f"  mov rsi, msg_{idx}\n"
+                        asm_code += f"  mov rdx, {len(string) + 1}\n"
+                        asm_code += f"  syscall\n\n"
                     else:
-                        raise ValueError(f"Line {idx} has the wrong format for outputting")
+                        content = variables.get(string, f"var_{idx}")
+                        asm_code += f"  mov rax, 1\n"
+                        asm_code += f"  mov rdi, 1\n"
+                        asm_code += f"  mov rsi, {content}\n"
+                        asm_code += f"  mov rdx, {len(string) + 1}\n"
+                        asm_code += f"  syscall\n\n"
+                else:
+                    raise ValueError(f"Line {idx} has the wrong format for outputting")
 
-                elif "exit" in part:
-                    asm_code += f"  mov rax, {exit_code}\n"
-                    asm_code += f"  mov rdi, rax\n"
-                    asm_code += f"  mov rax, 60\n"
-                    asm_code += f"  syscall\n"
+            elif "exit" in line:
+                asm_code += f"  mov rax, {exit_code}\n"
+                asm_code += f"  mov rdi, rax\n"
+                asm_code += f"  mov rax, 60\n"
+                asm_code += f"  syscall\n"
 
     return asm_code
