a120ac6a8f1f7b585e06fdc42f4119a1acd07c29
david <david.andrei.pisoi@gmail.com>
2024-05-07 18:59:14 +0300

    fixed major bug

--- src/compilefiles.py
+++ src/compilefiles.py
@@ -19,7 +19,7 @@ def compile_file(input_file):
     subprocess.run(['nasm', '-f', 'elf64', asm_file, '-o', obj_file], check=True)
     subprocess.run(['ld', obj_file, '-o', f"{exe_file}"], check=True)
 
-    os.remove(asm_file)
+    # os.remove(asm_file)
     os.remove(obj_file)
 
     return exe_file
--- src/generateassembly.py
+++ src/generateassembly.py
@@ -1,6 +1,6 @@
 import re
 
-def generate_asm(lines):
+def generate_asm(lines) -> str:
     asm_code = """global _start
 
 section .data\n"""
@@ -25,19 +25,28 @@ section .data\n"""
                 break
             elif "out" in line:
                 if "(" in line and ")" in line:
-                    string = re.search(r'out\((.*?)\)', line).group(1)
-                    if string.startswith('"') and string.endswith('"'):
-                        string = string[1:-1]
+                    output_content = re.search(r'out\((.*?)\)', line).group(1).strip()
+                    if output_content.startswith('"') and output_content.endswith('"'):
+                        string = output_content[1:-1]
+                        variables[f"msg_{idx}"] = string
                         asm_code += f"msg_{idx} db '{string}', 0xA, 0\n"
                     else:
-                        content = variables.get(string, f"var_{len(variables)}")
-                        asm_code += f"{content} db '{string}', 0xA, 0\n"
+                        if output_content in variables:
+                            pass
+                        else:
+                            raise ValueError(f"Variable '{output_content}' not declared")
                 else:
                     raise ValueError(f"Line {idx} has the wrong format for outputting")
             elif "var" in line:
                 var_name = line.split()[1].rstrip(";")
                 var_value = re.search(r'("[^"]*"|\d+)', line).group(1)
-                variables[var_name] = f"var_{idx}"
+                if var_value.startswith('"') and var_value.endswith('"'):
+                    var_value = var_value[1:-1]
+                if var_name not in variables:
+                    variables[var_name] = var_value
+                    asm_code += f"{var_name} db '{var_value}', 0xA, 0\n"
+                else:
+                    raise ValueError(f"Variable '{var_name}' already declared")
             elif "exit" in line:
                 exit_code = line.strip().split(" ")[1].rstrip(';')
 
@@ -73,11 +82,13 @@ _start:\n"""
                         asm_code += f"  mov rdx, {len(string) + 1}\n"
                         asm_code += f"  syscall\n\n"
                     else:
-                        content = variables.get(string, f"var_{idx}")
+                        content = variables.get(string)
+                        if content is None:
+                            raise ValueError(f"Variable '{string}' not declared")
                         asm_code += f"  mov rax, 1\n"
                         asm_code += f"  mov rdi, 1\n"
-                        asm_code += f"  mov rsi, {content}\n"
-                        asm_code += f"  mov rdx, {len(string) + 1}\n"
+                        asm_code += f"  mov rsi, {var_name}\n"
+                        asm_code += f"  mov rdx, {len(content) + 1}\n"
                         asm_code += f"  syscall\n\n"
                 else:
                     raise ValueError(f"Line {idx} has the wrong format for outputting")
