7e361b7d0260d0e77689dff61edf2e4dc8bb87f9
david <david.andrei.pisoi@gmail.com>
2024-05-07 14:54:02 +0300

    maybe windows fixed

--- compiler.py
+++ compiler.py
@@ -147,8 +147,8 @@ def generate_asm(lines):
 def compile_file(input_file):
     output_file = os.path.splitext(input_file)[0]
     asm_file = output_file + '.asm'
-    obj_file = output_file + '.obj'
-    if (os.name == "nt"):
+    obj_file = output_file + '.o'
+    if os.name == 'nt':
         exe_file = output_file + '.exe'
     else:
         exe_file = output_file + '.out'
@@ -164,7 +164,7 @@ def compile_file(input_file):
     
     if os.name == 'nt':
         subprocess.run(['nasm.exe', '-f', 'win32', asm_file, '-o', obj_file], check=True)
-        subprocess.run(['link', f".\\obj_file", 'kernel32.lib', '/subsystem:console'], check=True)
+        subprocess.run(['link', '/subsystem:console', '/entry:main', '/out:' + exe_file, obj_file, 'kernel32.lib'], check=True)
     else:
         subprocess.run(['nasm', '-f', 'elf64', asm_file, '-o', obj_file], check=True)
         subprocess.run(['ld', obj_file, '-o', exe_file], check=True)
