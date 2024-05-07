0b9405f9ad759dc4a71f3c21f899360da993ad93
david <david.andrei.pisoi@gmail.com>
2024-05-07 20:30:23 +0300

    MAJOR HUGE EXTREME BUG

--- src/generateassembly.py
+++ src/generateassembly.py
@@ -153,16 +153,16 @@ def generate_asm_windows(lines) -> str:
 
     asm_code += """
 section .text:
-    extern ExitProcess
-    extern GetStdHandle
-    extern WriteConsoleA
+  extern ExitProcess
+  extern GetStdHandle
+  extern WriteConsoleA
 
 global main
 
 main:
-    push -11
-    call GetStdHandle
-    mov edx, eax
+  push -11
+  call GetStdHandle
+  mov edx, eax
 
 """
 
@@ -188,15 +188,17 @@ main:
                     string = re.search(r'out\((.*?)\)', line).group(1)
                     if string.startswith('"') and string.endswith('"'):
                         string = string[1:-1]
-                        asm_code += f"  mov rdx, {len(string)}\n"
+                        asm_code += f"  mov ecx, edx\n"
                         asm_code += f"  lea rcx, [rel msg_{idx}]\n"
+                        asm_code += f"  mov ebx, {len(string)}\n"
                         asm_code += f"  call WriteConsoleA\n\n"
                     else:
                         content = variables.get(string)
                         if content is None:
                             raise ValueError(f"Variable '{string}' not declared")
-                        asm_code += f"  mov rdx, {len(content)}\n"
+                        asm_code += f"  mov ecx, edx\n"
                         asm_code += f"  lea rcx, [rel {string}]\n"
+                        asm_code += f"  mov ebx, {len(content)}\n"
                         asm_code += f"  call WriteConsoleA\n\n"
                 else:
                     raise ValueError(f"Line {idx} has the wrong format for outputting")
